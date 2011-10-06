518ad0177ae2f1aaefc49285b3536a6bd8d9973c
Mark Adler <madler@alumni.caltech.edu>
2011-10-05 20:32:32 -0700

    Get inffixed.h and MAKEFIXED result to match.

--- inflate.c
+++ inflate.c
@@ -321,8 +321,8 @@ void makefixed()
     low = 0;
     for (;;) {
         if ((low % 7) == 0) printf("\n        ");
-        printf("{%u,%u,%d}", state.lencode[low].op, state.lencode[low].bits,
-               state.lencode[low].val);
+        printf("{%u,%u,%d}", (low & 127) == 99 ? 64 : state.lencode[low].op,
+               state.lencode[low].bits, state.lencode[low].val);
         if (++low == size) break;
         putchar(',');
     }
