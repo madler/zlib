de6cb6fe5c0b209c0337a78294017c38efd54c6b
Mark Adler <madler@alumni.caltech.edu>
2012-01-21 11:54:52 -0800

    Avoid some warnings in contrib/minizip [Vollant].

--- contrib/minizip/ioapi.c
+++ contrib/minizip/ioapi.c
@@ -10,11 +10,11 @@
 
 */
 
-#if (defined(_WIN32))
+#if defined(_WIN32) && (!(defined(_CRT_SECURE_NO_WARNINGS)))
         #define _CRT_SECURE_NO_WARNINGS
 #endif
 
-#ifdef __APPLE__
+#if defined(__APPLE__) || defined(IOAPI_NO_64)
 // In darwin and perhaps other BSD variants off_t is a 64 bit value, hence no need for specific 64 bit functions
 #define FOPEN_FUNC(filename, mode) fopen(filename, mode)
 #define FTELLO_FUNC(stream) ftello(stream)
