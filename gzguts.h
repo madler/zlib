3f4339b61b3737f6efdd55368488f0a5888e4935
Mark Adler <madler@alumni.caltech.edu>
2012-02-19 23:31:55 -0800

    Improve the detection of no hidden visibility attribute.

--- gzguts.h
+++ gzguts.h
@@ -12,8 +12,7 @@
 #  endif
 #endif
 
-#define GCC_VER ((__GNUC__-0) * 100 + __GNUC_MINOR__-0)
-#if ((GCC_VER >= 303 && !defined(__APPLE__)) || GCC_VER >= 400) && !defined(NO_VIZ)
+#if ((__GNUC__-0) * 100 + __GNUC_MINOR__-0) >= 303 && !defined(NO_VIZ)
 #  define ZLIB_INTERNAL __attribute__((visibility ("hidden")))
 #else
 #  define ZLIB_INTERNAL
