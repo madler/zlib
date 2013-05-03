ceeb615f4660966c192edb302207842c7283b5ac
Mark Adler <madler@alumni.caltech.edu>
2013-05-02 23:12:54 -0700

    Change version number to 1.2.8.1.

--- contrib/vstudio/readme.txt
+++ contrib/vstudio/readme.txt
@@ -1,4 +1,4 @@
-Building instructions for the DLL versions of Zlib 1.2.8
+Building instructions for the DLL versions of Zlib 1.2.8.1
 ========================================================
 
 This directory contains projects that build zlib and minizip using
--- zlib.h
+++ zlib.h
@@ -1,5 +1,5 @@
 /* zlib.h -- interface of the 'zlib' general purpose compression library
-  version 1.2.8, April 28th, 2013
+  version 1.2.8.1, May xxth, 2013
 
   Copyright (C) 1995-2013 Jean-loup Gailly and Mark Adler
 
@@ -37,12 +37,12 @@
 extern "C" {
 #endif
 
-#define ZLIB_VERSION "1.2.8"
-#define ZLIB_VERNUM 0x1280
+#define ZLIB_VERSION "1.2.8.1-motley"
+#define ZLIB_VERNUM 0x1281
 #define ZLIB_VER_MAJOR 1
 #define ZLIB_VER_MINOR 2
 #define ZLIB_VER_REVISION 8
-#define ZLIB_VER_SUBREVISION 0
+#define ZLIB_VER_SUBREVISION 1
 
 /*
     The 'zlib' compression library provides in-memory compression and
