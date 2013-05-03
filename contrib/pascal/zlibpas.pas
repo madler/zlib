d9e307c075eac2eaa808644c30cbacb3f45546bb
Mark Adler <madler@alumni.caltech.edu>
2013-05-02 23:12:54 -0700

    Change version number to 1.2.8.1.

--- contrib/pascal/zlibpas.pas
+++ contrib/pascal/zlibpas.pas
@@ -10,8 +10,8 @@ unit zlibpas;
 interface
 
 const
-  ZLIB_VERSION = '1.2.8';
-  ZLIB_VERNUM  = $1280;
+  ZLIB_VERSION = '1.2.8.1';
+  ZLIB_VERNUM  = $1281;
 
 type
   alloc_func = function(opaque: Pointer; items, size: Integer): Pointer;
