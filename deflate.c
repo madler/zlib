31043308c3d3edfb487d2c4cbe7290bd5b63c65c
Shuxin Yang <shuxinyang2006@gmail.com>
2014-03-18 11:15:06 -0700

    longest_match() implementation for x86-family platform.
    See the header of "contrib/amd64/longest-match.inc" for details

--- deflate.c
+++ deflate.c
@@ -1153,6 +1153,10 @@ local void lm_init (s)
  * OUT assertion: the match length is not greater than s->lookahead.
  */
 #ifndef ASMV
+
+#if (defined(__i386) || defined(__x86_64)) && !defined(NOT_TWEAK_COMPILER)
+#include "contrib/amd64/longest-match.inc"
+#else
 /* For 80x86 and 680x0, an optimized version will be provided in match.asm or
  * match.S. The code will be functionally equivalent.
  */
@@ -1298,6 +1302,7 @@ local uInt longest_match(s, cur_match)
     if ((uInt)best_len <= s->lookahead) return (uInt)best_len;
     return s->lookahead;
 }
+#endif
 #endif /* ASMV */
 
 #else /* FASTEST */
