f48085006c90e236d46ef0f319db6267cf96131e
gnieboer <gnieboer@corpcomm.net>
2016-01-30 11:03:52 +0300

    VS2015 compatibility, including project files, AVX2-only option, ml compatibility, and snprintf inclusion

--- contrib/masmx86/bld_ml32.bat
+++ contrib/masmx86/bld_ml32.bat
@@ -1,2 +1,2 @@
-ml /coff /Zi /c /Flmatch686.lst match686.asm
-ml /coff /Zi /c /Flinffas32.lst inffas32.asm
+ml /coff /Zi /c /safeseh /Flmatch686.lst match686.asm
+ml /coff /Zi /c /safeseh /Flinffas32.lst inffas32.asm
