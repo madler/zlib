33a7aff45da511c3fc181071394ad9a01d5e8d61
Mark Adler <madler@alumni.caltech.edu>
2016-09-21 08:45:59 -0700

    Fix typo.

--- inflate.h
+++ inflate.h
@@ -78,7 +78,7 @@ typedef enum {
  */
 
 /* State maintained between inflate() calls -- approximately 7K bytes, not
-   inlcuding the allocated sliding window, which is up to 32K bytes. */
+   including the allocated sliding window, which is up to 32K bytes. */
 struct inflate_state {
     inflate_mode mode;          /* current inflate mode */
     int last;                   /* true if processing last block */
