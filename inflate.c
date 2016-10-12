ffbc5602641acee7030a128f5618aa09c37ac786
Mark Adler <madler@alumni.caltech.edu>
2016-10-11 18:38:20 -0700

    Avoid casting an out-of-range value to long.

--- inflate.c
+++ inflate.c
@@ -1526,7 +1526,7 @@ z_streamp strm;
     struct inflate_state FAR *state;
 
     if (strm == Z_NULL || strm->state == Z_NULL)
-        return (long)(((unsigned long)0 - 1) << 16);
+        return -(1L << 16);
     state = (struct inflate_state FAR *)strm->state;
     return (long)(((unsigned long)((long)state->back)) << 16) +
         (state->mode == COPY ? state->length :
