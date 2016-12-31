354c29d3b8fb11d33caad90d326754edc1408e3f
Mark Adler <madler@alumni.caltech.edu>
2016-12-31 08:49:17 -0800

    Use a uniform approach for the largest value of an unsigned type.

--- inflate.c
+++ inflate.c
@@ -1555,7 +1555,7 @@ unsigned long ZEXPORT inflateCodesUsed(strm)
 z_streamp strm;
 {
     struct inflate_state FAR *state;
-    if (inflateStateCheck(strm)) return (unsigned long)0 - 1;
+    if (inflateStateCheck(strm)) return (unsigned long)-1;
     state = (struct inflate_state FAR *)strm->state;
     return (unsigned long)(state->next - state->codes);
 }
