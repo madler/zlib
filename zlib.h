936d63ae94197a598a7c030c255057a2e73eee09
Mark Adler <madler@alumni.caltech.edu>
2017-01-15 09:18:46 -0800

    zlib 1.2.11

--- zlib.h
+++ zlib.h
@@ -1,5 +1,5 @@
 /* zlib.h -- interface of the 'zlib' general purpose compression library
-  version 1.2.10.1, January 3rd, 2017
+  version 1.2.11, January 15th, 2017
 
   Copyright (C) 1995-2017 Jean-loup Gailly and Mark Adler
 
@@ -37,12 +37,12 @@
 extern "C" {
 #endif
 
-#define ZLIB_VERSION "1.2.10.1-motley"
-#define ZLIB_VERNUM 0x12a1
+#define ZLIB_VERSION "1.2.11"
+#define ZLIB_VERNUM 0x12b0
 #define ZLIB_VER_MAJOR 1
 #define ZLIB_VER_MINOR 2
-#define ZLIB_VER_REVISION 10
-#define ZLIB_VER_SUBREVISION 1
+#define ZLIB_VER_REVISION 11
+#define ZLIB_VER_SUBREVISION 0
 
 /*
     The 'zlib' compression library provides in-memory compression and
