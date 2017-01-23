d894351ab113b4eceee79f272914252fc3a03177
Sylvestre Ledru <sylvestre@debian.org>
2017-01-23 08:00:15 +0100

    Fix coverity defect 1399504 - Nesting level does not match indentation. Only the first declaration of the macro will be executed in the first, the rest outside of the if

--- deflate.c
+++ deflate.c
@@ -189,9 +189,10 @@ local const config configuration_table[10] = {
  * Initialize the hash table (avoiding 64K overflow for 16 bit systems).
  * prev[] will be initialized on the fly.
  */
-#define CLEAR_HASH(s) \
+#define CLEAR_HASH(s) {\
     s->head[s->hash_size-1] = NIL; \
-    zmemzero((Bytef *)s->head, (unsigned)(s->hash_size-1)*sizeof(*s->head));
+    zmemzero((Bytef *)s->head, (unsigned)(s->hash_size-1)*sizeof(*s->head)); \
+}
 
 /* ===========================================================================
  * Slide the hash table when sliding the window down (could be avoided with 32
