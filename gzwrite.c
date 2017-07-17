8cc7968fd4d2ec47ff8bedf654f2458fc5d2dff3
Matt Kulukundis <matt.fowles@gmail.com>
2017-07-17 16:40:00 -0400

    Add explicit checks and casts for conversions
    Two already safe:
    gzwrite.c(212): warning C4267: '=': conversion from 'size_t' to 'unsigned int', possible loss of data
    gzwrite.c(232): warning C4267: '=': conversion from 'size_t' to 'unsigned int', possible loss of data
    
    One requires more argument validation:
    gzwrite.c(371): warning C4267: '=': conversion from 'size_t' to 'int', possible loss of data

--- gzwrite.c
+++ gzwrite.c
@@ -209,7 +209,7 @@ local z_size_t gz_write(state, buf, len)
                               state->in);
             copy = state->size - have;
             if (copy > len)
-                copy = len;
+                copy = (unsigned)len;
             memcpy(state->in + have, buf, copy);
             state->strm.avail_in += copy;
             state->x.pos += copy;
@@ -229,7 +229,7 @@ local z_size_t gz_write(state, buf, len)
         do {
             unsigned n = (unsigned)-1;
             if (n > len)
-                n = len;
+                n = (unsigned)len;
             state->strm.avail_in = n;
             state->x.pos += n;
             if (gz_comp(state, Z_NO_FLUSH) == -1)
@@ -366,10 +366,17 @@ int ZEXPORT gzputs(file, str)
     if (state->mode != GZ_WRITE || state->err != Z_OK)
         return -1;
 
-    /* write string */
+    /* since an int is returned, make sure len fits in one, otherwise return
+       with an error (this avoids a flaw in the interface) */
     len = strlen(str);
-    ret = gz_write(state, str, len);
-    return ret == 0 && len != 0 ? -1 : ret;
+    if (len > INT_MAX) {
+        gz_error(state, Z_DATA_ERROR, "requested length does not fit in int");
+        return -1;
+    }
+
+    /* write len bytes from buf (the return value will fit in an int) */
+    ret = (int)gz_write(state, str, len);
+    return ret < len ? -1 : ret;
 }
 
 #if defined(STDC) || defined(Z_HAVE_STDARG_H)
