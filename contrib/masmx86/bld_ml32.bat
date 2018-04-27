e728a74d76ce408c1aa442c5e261f76a862613b6
Noah Davidson <noahd@ntdev.microsoft.com>
2018-04-27 16:49:53 -0700

    added /safeseh to ml inorder to allow linking obj files for x86

--- contrib/masmx86/bld_ml32.bat
+++ contrib/masmx86/bld_ml32.bat
@@ -1,2 +1,2 @@
-ml /coff /Zi /c /Flmatch686.lst match686.asm
-ml /coff /Zi /c /Flinffas32.lst inffas32.asm
+ml /coff /Zi /safeseh /c /Flmatch686.lst match686.asm
+ml /coff /Zi /safeseh /c /Flinffas32.lst inffas32.asm
