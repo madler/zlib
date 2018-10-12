c41632ff0a8a432047d073e028a1ae05d2df979d
clin1234 <clin@sr.com>
2018-10-12 07:42:03 -0400

    Revert "Add fallthrough comment"
    This reverts commit 1545a18e97caf01b4b93e86eb2c5dd910e00719a.

--- infback.c
+++ infback.c
@@ -477,7 +477,6 @@ void FAR *out_desc;
             }
             Tracev((stderr, "inflate:       codes ok\n"));
             state->mode = LEN;
-            /* fallthrough */
 
         case LEN:
             /* use inflate_fast() if we have enough input and output */
