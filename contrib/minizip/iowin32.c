c7e1158de76bb61444f00f95089f365ea6f2d1f8
Jamie Madill <jmadill@chromium.org>
2020-03-17 09:01:14 -0400

    Fix UWP detection in iowin32.c.
    Use WINAPI_FAMILY instead of WINAPI_FAMILY_PARTITION.
    
    Fixes #480

--- contrib/minizip/iowin32.c
+++ contrib/minizip/iowin32.c
@@ -26,9 +26,8 @@
 #endif
 
 
-// see Include/shared/winapifamily.h in the Windows Kit
-#if defined(WINAPI_FAMILY_PARTITION) && (!(defined(IOWIN32_USING_WINRT_API)))
-#if WINAPI_FAMILY_ONE_PARTITION(WINAPI_FAMILY, WINAPI_PARTITION_APP)
+#if !defined(IOWIN32_USING_WINRT_API)
+#if defined(WINAPI_FAMILY) && (WINAPI_FAMILY == WINAPI_FAMILY_APP)
 #define IOWIN32_USING_WINRT_API 1
 #endif
 #endif
