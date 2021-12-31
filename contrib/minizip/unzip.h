05ff9aa2bb752e5a88d0097e36623c9d662fbee3
Anders <anders_k@users.sourceforge.net>
2021-12-31 20:16:44 +0100

    Fix typos

--- contrib/minizip/unzip.h
+++ contrib/minizip/unzip.h
@@ -154,12 +154,12 @@ extern int ZEXPORT unzStringFileNameCompare OF ((const char* fileName1,
                                                  const char* fileName2,
                                                  int iCaseSensitivity));
 /*
-   Compare two filename (fileName1,fileName2).
-   If iCaseSenisivity = 1, comparision is case sensitivity (like strcmp)
-   If iCaseSenisivity = 2, comparision is not case sensitivity (like strcmpi
+   Compare two filenames (fileName1,fileName2).
+   If iCaseSensitivity = 1, comparison is case sensitive (like strcmp)
+   If iCaseSensitivity = 2, comparison is not case sensitive (like strcmpi
                                 or strcasecmp)
-   If iCaseSenisivity = 0, case sensitivity is defaut of your operating system
-    (like 1 on Unix, 2 on Windows)
+   If iCaseSensitivity = 0, case sensitivity is the default from your 
+     operating system (like 1 on Unix, 2 on Windows)
 */
 
 
