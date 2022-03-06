f06d2161e3d329c7ca2aaba72b923e46113d3052
Ken Matsui <26405363+ken-matsui@users.noreply.github.com>
2022-03-06 16:22:59 +0900

    Fix code scanning alert - Multiplication result converted to larger type
    Tracking issue for: https://github.com/poacpm/poac/security/code-scanning/1

--- zutil.c
+++ zutil.c
@@ -308,7 +308,7 @@ voidpf ZLIB_INTERNAL zcalloc (opaque, items, size)
     unsigned size;
 {
     (void)opaque;
-    return sizeof(uInt) > 2 ? (voidpf)malloc(items * size) :
+    return sizeof(uInt) > 2 ? (voidpf)malloc((unsigned long)items * size) :
                               (voidpf)calloc(items, size);
 }
 
