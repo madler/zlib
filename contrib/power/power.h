2b843de4ffa241edc20877f3ec47e75c9ed13ec8
Manjunath S Matti <mmatti@linux.ibm.com>
2022-11-16 03:53:53 -0600

    Add Power8+ optimized crc32
    This commit adds an optimized version for the crc32 function based
    on crc32-vpmsum from https://github.com/antonblanchard/crc32-vpmsum/
    
    This is the C implementation created by Rogerio Alves
    <rogealve@br.ibm.com>
    
    It makes use of vector instructions to speed up CRC32 algorithm.
    
    Author: Rogerio Alves <rcardoso@linux.ibm.com>
    Signed-off-by: Manjunath Matti <mmatti@linux.ibm.com>

--- contrib/power/power.h
+++ contrib/power/power.h
@@ -2,3 +2,7 @@
  *               2019 Rogerio Alves    <rogerio.alves@ibm.com>, IBM
  * For conditions of distribution and use, see copyright notice in zlib.h
  */
+
+#include "../../zconf.h"
+
+unsigned long _crc32_z_power8(unsigned long, const Bytef *, z_size_t);
