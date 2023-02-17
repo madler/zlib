a77a9788dba9ff360f3a518bed3dcc290e170ced
Mark Adler <madler@alumni.caltech.edu>
2023-02-17 00:06:32 -0800

    Assure that inflatePrime() can't shift a 32-bit integer by 32 bits.
    The inflate() functions never leave state->bits greater than 24, so
    an inflatePrime() call could not cause this. The only way this
    could have happened would be by using inflatePrime() to fill the
    bit buffer with 32 bits, and then calling inflatePrime() a *second*
    time asking to insert zero bits, for some reason. This commit
    assures that a shift by 32 bits does not occur even in that case.

--- inflate.c
+++ inflate.c
@@ -241,6 +241,8 @@ int ZEXPORT inflatePrime(z_streamp strm, int bits, int value) {
     struct inflate_state FAR *state;
 
     if (inflateStateCheck(strm)) return Z_STREAM_ERROR;
+    if (bits == 0)
+        return Z_OK;
     state = (struct inflate_state FAR *)strm->state;
     if (bits < 0) {
         state->hold = 0;
