0a71628b1fba6d644a03bf3fa3f72c11f5d446d2
Gilles Vollant <info@winimage.com>
2023-07-29 11:17:19 +0200

    minizip support for Haiku #741

--- contrib/minizip/ioapi.h
+++ contrib/minizip/ioapi.h
@@ -50,7 +50,7 @@
 #define ftello64 ftell
 #define fseeko64 fseek
 #else
-#if defined(__FreeBSD__) || defined(__OpenBSD__) || defined(__NetBSD__)
+#if defined(__FreeBSD__) || defined(__OpenBSD__) || defined(__NetBSD__) || defined(__HAIKU__) || defined(MINIZIP_FOPEN_NO_64)
 #define fopen64 fopen
 #define ftello64 ftello
 #define fseeko64 fseeko
