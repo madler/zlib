096441298ecd1c123f1d37c2b34d6b6bb3c42e93
Manjunath S Matti <mmatti@linux.ibm.com>
2023-09-14 06:15:57 -0500

    Preparation for Power optimizations
    Optimized functions for Power will make use of GNU indirect functions,
    an extension to support different implementations of the same function,
    which can be selected during runtime. This will be used to provide
    optimized functions for different processor versions.
    
    Since this is a GNU extension, we placed the definition of the Z_IFUNC
    macro under `contrib/gcc`. This can be reused by other archs as well.
    
    Author: Matheus Castanho <msc@linux.ibm.com>
    Author: Rogerio Alves <rcardoso@linux.ibm.com>
    Signed-off-by: Manjunath Matti <mmatti@linux.ibm.com>

---
+++ contrib/power/power.h
@@ -0,0 +1,4 @@
+/* Copyright (C) 2019 Matheus Castanho <msc@linux.ibm.com>, IBM
+ *               2019 Rogerio Alves    <rogerio.alves@ibm.com>, IBM
+ * For conditions of distribution and use, see copyright notice in zlib.h
+ */
