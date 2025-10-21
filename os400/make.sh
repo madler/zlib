cde4c54f7eff5818d1cb91794d485e10b213f360
Chi-Iroh <69324626+Chi-Iroh@users.noreply.github.com>
2025-10-21 13:49:27 +0200

    [OS400] Copy free form header to the source file

--- os400/make.sh
+++ os400/make.sh
@@ -247,6 +247,18 @@ then    CMD="CPY OBJ('${HFILE}') TOOBJ('${DEST}')"
         # touch -r "${HFILE}" "${DEST}"
 fi
 
+HFILE="${SCRIPTDIR}/zlibfree"
+MBR="ZLIBFREE"
+DEST="${SRCPF}/${MBR}.MBR"
+
+if action_needed "${DEST}" "${HFILE}"
+then    CMD="CPY OBJ('${HFILE}') TOOBJ('${DEST}')"
+        CMD="${CMD} TOCCSID(${TGTCCSID}) DTAFMT(*TEXT) REPLACE(*YES)"
+        system "${CMD}"
+        # touch -r "${HFILE}" "${DEST}"
+        system "CHGPFM FILE(${TARGETLIB}/H) MBR(${MBR}) SRCTYPE(RPGLE)"
+fi
+
 IFSFILE="${IFSDIR}/include/`basename \"${HFILE}\"`"
 
 if action_needed "${IFSFILE}" "${DEST}"
