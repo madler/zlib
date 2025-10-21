82a85bbeb156631465f53ad855b9cf963d441154
Chi-Iroh <69324626+Chi-Iroh@users.noreply.github.com>
2025-10-21 13:37:59 +0200

    [OS400] Gave type RPGLE to include member ZLIB.INC

--- os400/make.sh
+++ os400/make.sh
@@ -238,13 +238,15 @@ done
 
 
 HFILE="${SCRIPTDIR}/zlib.inc"
-DEST="${SRCPF}/ZLIB.INC.MBR"
+MBR="ZLIB.INC"
+DEST="${SRCPF}/${MBR}.MBR"
 
 if action_needed "${DEST}" "${HFILE}"
 then    CMD="CPY OBJ('${HFILE}') TOOBJ('${DEST}')"
         CMD="${CMD} TOCCSID(${TGTCCSID}) DTAFMT(*TEXT) REPLACE(*YES)"
         system "${CMD}"
         # touch -r "${HFILE}" "${DEST}"
+        system "CHGPFM FILE(${TARGETLIB}/H) MBR(${MBR}) SRCTYPE(RPGLE)"
 fi
 
 IFSFILE="${IFSDIR}/include/`basename \"${HFILE}\"`"
